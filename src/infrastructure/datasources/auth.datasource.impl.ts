eaad1278ccba6a692f3a70c6173758dc1eec4abe
Ignacio H <55958716+kiltro-dev@users.noreply.github.com>
2023-12-08 16:59:27 -0300

    chore: implement datasource

---
+++ src/infrastructure/datasources/auth.datasource.impl.ts
@@ -0,0 +1,20 @@
+import { AuthDataSource } from '../../domain/datasources';
+import { RegisterUserDto } from '../../domain/dtos';
+import { UserEntity } from '../../domain/entities';
+import { CustomError } from '../../domain/errors';
+
+export class AuthDataSourceImpl implements AuthDataSource {
+  register(registerUserDto: RegisterUserDto): Promise<UserEntity> {
+    const { name, email, password } = registerUserDto;
+    try {
+      return Promise.resolve(
+        new UserEntity('1', name, email, password, ['ADMIN_ROLE']),
+      );
+    } catch (error) {
+      if (error instanceof CustomError) {
+        throw error;
+      }
+      throw CustomError.internalServer();
+    }
+  }
+}
