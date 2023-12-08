51f74e81e7f76cbbd8b8913c1bfaf4c220757256
Ignacio H <55958716+kiltro-dev@users.noreply.github.com>
2023-12-08 13:38:16 -0300

    chore: install express and setup server

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
-console.log('Hello Clean Architecture!');
+import { Server } from './presentation/server';
+
+(() => {
+  main();
+})();
+async function main() {
+  // TODO: await const
+  // TODO: server init
+  new Server({}).start();
+}
---
+++ src/presentation/server.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+
+interface Options {
+  port?: number;
+}
+
+export class Server {
+  public readonly app = express();
+  private readonly port: number;
+  constructor(options: Options) {
+    const { port = 3100 } = options;
+    this.port = port;
+  }
+  async start() {
+    this.app.listen(3000, () => {
+      console.log(`Server running on port ${this.port}`);
+    });
+  }
+}
