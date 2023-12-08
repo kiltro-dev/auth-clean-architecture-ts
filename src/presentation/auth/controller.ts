22550e4fb40f554192df5b005eea7a0a542c5e16
Ignacio H <55958716+kiltro-dev@users.noreply.github.com>
2023-12-08 15:56:20 -0300

    chore: use dto

--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -1,10 +1,13 @@
 import { Request, Response } from 'express';
+import { RegisterUserDto } from '../../domain/dtos';
 
 export class AuthController {
   constructor() {}
 
   registerUser = (req: Request, res: Response) => {
-    res.json('register user controller');
+    const [error, registerUserDto] = RegisterUserDto.create(req.body);
+    if (error) return res.status(400).json({ error });
+    res.json(registerUserDto);
   };
 
   loginUser = (req: Request, res: Response) => {
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -17,6 +17,8 @@ export class Server {
     this.routes = routes;
   }
   async start() {
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
     this.app.use(this.routes);
     this.app.listen(this.port, () => {
       console.log(`Server running on port ${this.port}`);
